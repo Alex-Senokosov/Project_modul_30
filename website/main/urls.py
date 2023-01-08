29e07bf9cfbe985de93e91532e375eae432655fa
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-08 20:45:01 +0300

    DZ23

--- website/main/urls.py
+++ website/main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.index),
     path('DZ/',views.DZ),
     path('about/',views.about),
-    path ("DZ2/",views.DZ2)
+    path ("DZ2/",views.DZ2),
+    path("DZ3/", views.DZ3),
+    path("DZ3X/", views.DZ3X)
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index (request):
-    return HttpResponse("<h4>Hello<h4>")
+    return render(request,"main/index.html")
 def DZ (request):
     return HttpResponse("<h4>ДЗ-готово<h4>")
 def about (request):
-    return HttpResponse("<h4>About<h4>")
+    return render(request,"main/about.html")
 def DZ2 (request):
     return render(request,"DZ2.html")
+
+def DZ3 (request):
+    return render(request,"DZ3.html")
+
+def DZ3X (request):
+    return render(request,"DZ3X.html")
