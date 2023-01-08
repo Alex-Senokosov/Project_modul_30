abf54d4b4181e6b5d02299bb308a988618f8b101
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-08 19:47:54 +0300

    DZ2

--- website/main/urls.py
+++ website/main/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.index),
-    path('DZ/',views.DZ)
+    path('DZ/',views.DZ),
+    path('about/',views.about),
+    path ("DZ2/",views.DZ2)
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -4,3 +4,7 @@ def index (request):
     return HttpResponse("<h4>Hello<h4>")
 def DZ (request):
     return HttpResponse("<h4>ДЗ-готово<h4>")
+def about (request):
+    return HttpResponse("<h4>About<h4>")
+def DZ2 (request):
+    return render(request,"DZ2.html")
