9ac94de7c3b70afd334020d49b7e7cb188e6def0
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-09 20:42:04 +0300

    jinja+DZ4

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField("Описание")
+    time = models.DateTimeField("Время записи",auto_now_add=True)
+    def __str__(self):
+        return self.title
--- website/main/urls.py
+++ website/main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.index),
-    path('DZ/',views.DZ),
-    path('about/',views.about),
-    path ("DZ2/",views.DZ2),
-    path("DZ3/", views.DZ3),
-    path("DZ3X/", views.DZ3X)
+    path('',views.index,name="home"),
+    path('DZ/',views.DZ,name="DZ1"),
+    path('about/',views.about,name="about"),
+    path ("DZ2/",views.DZ2,name="DZ2"),
+    path("DZ3/", views.DZ3,name="DZ3"),
+    path("DZ3X/", views.DZ3X,name="DZ3")
 ]
