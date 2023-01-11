6b3cb0f90bcac7130087760058df94f5ef2bbecd
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-11 21:02:36 +0300

    DZ5

--- website/main/models.py
+++ website/main/models.py
@@ -4,5 +4,11 @@ class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField("Описание")
     time = models.DateTimeField("Время записи",auto_now_add=True)
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 def index (request):
-    return render(request,"main/index.html")
+    task = Task.objects.all()
+    return render(request,"main/index.html",{"title":"Главная страница","tasks": task})
+
 def DZ (request):
     return HttpResponse("<h4>ДЗ-готово<h4>")
 def about (request):
