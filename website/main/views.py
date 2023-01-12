0b80d5fb379c60d7eb7657ee37c01a695c3659de
alexsenokosov <alexsenokosov666@gmail.com>
2023-01-12 19:52:06 +0300

    form

--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 def index (request):
     task = Task.objects.order_by("-id")
     return render(request,"main/index.html",{"title":"Главная страница","tasks": task})
@@ -18,4 +19,6 @@ def DZ3 (request):
 def DZ3X (request):
     return render(request,"DZ3X.html")
 def create(request):
+    form = TaskForm ()
+    context={"form":form}
     return render(request,"main/create.html")
